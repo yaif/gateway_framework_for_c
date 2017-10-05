dc8f0513bd8f1250feac8979391248fb4c510358
Cheng Tang <ctang@thoughtworks.com>
2017-10-06 00:15:37 +0800

    use class and polymorphism to test lw_oopc.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include "iot_platform/AWSIoTPlatform.h"
 
 int main(int argc, char *argv[])
 {
+    AWSIoTPlatform* aws_iot_platform = AWSIoTPlatform_new();
+    aws_iot_platform->init(aws_iot_platform);
+    IoTPlatform* iot_platform = SUPER_PTR(aws_iot_platform, IoTPlatform);
+    printf("%s\n", iot_platform->get_name(iot_platform));
+    lw_oopc_delete(aws_iot_platform);
     return 0;
 }
